b94a4157057ae393b6fecd4785b2bb11799c897d
Timur Cerimagic <tcerimagic@gmail.com>
2017-05-31 17:04:39 +0200

    Pilot

---
+++ app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {ProductListComponent} from './products/product-list.component'
+import { AppComponent }  from './app.component';
+import {FormsModule} from '@angular/forms';
+import {ProductFilterPipe} from './products/product-filter.pipe';
+
+@NgModule({
+  imports: [ BrowserModule,
+  FormsModule],
+  declarations: [ AppComponent,
+   ProductListComponent,
+   ProductFilterPipe],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
