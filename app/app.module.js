43e97e349577e479f3201e3111254ce4fcb714dc
Emin Šehić <emin.sehic13@gmail.com>
2017-06-12 12:48:36 +0200

    neam pojma

--- app/app.module.js
+++ app/app.module.js
@@ -9,7 +9,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var platform_browser_1 = require("@angular/platform-browser");
 var app_component_1 = require("./app.component");
-var forms_1 = require("@angular/forms");
 var AppModule = (function () {
     function AppModule() {
     }
@@ -17,8 +16,7 @@ var AppModule = (function () {
 }());
 AppModule = __decorate([
     core_1.NgModule({
-        imports: [platform_browser_1.BrowserModule,
-            forms_1.FormsModule],
+        imports: [platform_browser_1.BrowserModule],
         declarations: [app_component_1.AppComponent],
         bootstrap: [app_component_1.AppComponent]
     })
