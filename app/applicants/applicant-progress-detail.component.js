1dc5ad4cfe6514d2c9051a0365b20093cf0a0051
emin.sehic <emin.sehic13@gmail.com>
2017-06-18 18:01:06 +0200

    progress-detail gotov

--- app/applicants/applicant-progress-detail.component.js
+++ app/applicants/applicant-progress-detail.component.js
@@ -5,19 +5,29 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+var applicant_service_1 = require("./applicant.service");
 var ApplicantProgressDetailComponent = (function () {
-    function ApplicantProgressDetailComponent() {
-        this.showUserInfo = true;
+    function ApplicantProgressDetailComponent(_applicantService) {
+        this._applicantService = _applicantService;
+        this.isActive = false;
     }
+    ApplicantProgressDetailComponent.prototype.searchByKey = function () {
+        var _this = this;
+        this._applicantService.getApplicantByKey(this.inputKey).subscribe(function (appllicant) { return _this.applicant = appllicant; }, function (error) { return _this.errorMessage = error; });
+    };
     return ApplicantProgressDetailComponent;
 }());
 ApplicantProgressDetailComponent = __decorate([
     core_1.Component({
         templateUrl: 'app/applicants/applicant-progress-detail.component.html',
         styleUrls: ['app/applicants/applicant-progress-detail.component.css']
-    })
+    }),
+    __metadata("design:paramtypes", [applicant_service_1.ApplicantService])
 ], ApplicantProgressDetailComponent);
 exports.ApplicantProgressDetailComponent = ApplicantProgressDetailComponent;
 //# sourceMappingURL=applicant-progress-detail.component.js.map
--- app/applicants/applicant-progress-detail.component.ts
+++ app/applicants/applicant-progress-detail.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 
 import { IApplicant } from './applicant';
+import { ApplicantService } from './applicant.service';
 
 @Component({
     templateUrl: 'app/applicants/applicant-progress-detail.component.html',
     styleUrls: ['app/applicants/applicant-progress-detail.component.css']
 })
 export class ApplicantProgressDetailComponent {
-    showUserInfo: boolean = true;
+    inputKey: string;
+    errorMessage: string;
+
+    isActive: boolean = false;
+
     applicant: IApplicant;
 
-    
+       
+    constructor(private _applicantService: ApplicantService) {
+    }
+
+    searchByKey(): void {        
+        this._applicantService.getApplicantByKey(this.inputKey).subscribe(appllicant => this.applicant = appllicant, error => this.errorMessage = <any>error);
+    }
 }
