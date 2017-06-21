69bc7a1d71618c3b034ff6e32734c67f7c925915
emin.sehic <emin.sehic13@gmail.com>
2017-06-21 08:11:37 +0200

    competition save komponenta

--- app/competitions/competition-save.component.js
+++ app/competitions/competition-save.component.js
@@ -5,17 +5,60 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+var router_1 = require("@angular/router");
+var competition_service_1 = require("./competition.service");
 var CompetitionSaveComponent = (function () {
-    function CompetitionSaveComponent() {
+    function CompetitionSaveComponent(_route, _router, _competitionService) {
+        this._route = _route;
+        this._router = _router;
+        this._competitionService = _competitionService;
+        this.competition = {
+            "competitionId": null,
+            "competitionTitle": "",
+            "codeBlockId": null,
+            "published": "",
+            "deadline": "",
+            "maxNumberApplicants": null,
+            "description": "",
+            "imageUrl": ""
+        };
+        this.enableButton = false;
+        this.competitionPositionMessage = "Competition position must be provided!";
     }
+    CompetitionSaveComponent.prototype.ngOnInit = function () { };
+    CompetitionSaveComponent.prototype.onBack = function () {
+        this._router.navigate(['/competitions']);
+    };
+    CompetitionSaveComponent.prototype.setMinDate = function () {
+        var today = new Date().toISOString().split('T')[0];
+        document.getElementById("startDate").setAttribute("min", today);
+        document.getElementById("dueDate").setAttribute("min", today);
+        if (this.competition.published != null) {
+            document.getElementById("dueDate").setAttribute("min", this.competition.published);
+        }
+        else if (this.competition.deadline != null) {
+            document.getElementById("startDate").setAttribute("max", this.competition.deadline);
+        }
+        else
+            document.getElementById("dueDate").setAttribute("min", today);
+    };
+    CompetitionSaveComponent.prototype.saveCompetition = function () {
+    };
     return CompetitionSaveComponent;
 }());
 CompetitionSaveComponent = __decorate([
     core_1.Component({
-        templateUrl: 'app/competitions/competition-save.component.html'
-    })
+        templateUrl: 'app/competitions/competition-save.component.html',
+        styleUrls: ['app/competitions/competition-save.component.css']
+    }),
+    __metadata("design:paramtypes", [router_1.ActivatedRoute,
+        router_1.Router,
+        competition_service_1.CompetitionService])
 ], CompetitionSaveComponent);
 exports.CompetitionSaveComponent = CompetitionSaveComponent;
 //# sourceMappingURL=competition-save.component.js.map
--- app/competitions/competition-save.component.ts
+++ app/competitions/competition-save.component.ts
@@ -1,8 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Subscription }       from 'rxjs/Subscription';
+
+import { ICompetition } from './competition';
+import { CompetitionService } from './competition.service';
 
 @Component({
-    templateUrl: 'app/competitions/competition-save.component.html'
+    templateUrl: 'app/competitions/competition-save.component.html',
+    styleUrls: ['app/competitions/competition-save.component.css']
 })
-export class CompetitionSaveComponent {
+export class CompetitionSaveComponent implements OnInit {
+
+    competition: ICompetition = {
+        "competitionId": null,
+        "competitionTitle" :"",
+        "codeBlockId": null,
+        "published": "",
+        "deadline": "",
+        "maxNumberApplicants": null,
+        "description" : "",
+        "imageUrl": ""
+    };
+   
+    private enableButton: boolean = false;
+    private competitionPositionMessage: string = "Competition position must be provided!";
     
+     constructor(private _route: ActivatedRoute,
+                private _router: Router,
+                private _competitionService: CompetitionService,) {
+    }
+
+      ngOnInit(): void {}
+
+     onBack(): void {
+        this._router.navigate(['/competitions']);
+    }
+
+    setMinDate() {
+        var today: string = new Date().toISOString().split('T')[0];
+        document.getElementById("startDate").setAttribute("min", today);
+        document.getElementById("dueDate").setAttribute("min", today);
+
+        if (this.competition.published != null) {
+            document.getElementById("dueDate").setAttribute("min", this.competition.published);
+        }
+        else if (this.competition.deadline != null) {
+            document.getElementById("startDate").setAttribute("max", this.competition.deadline);
+        }
+        else
+            document.getElementById("dueDate").setAttribute("min", today);
+    }
+
+    saveCompetition(): void {
+
+    }
 }
